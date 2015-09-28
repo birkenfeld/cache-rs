6dbf4eba676507e959b999acf58cdafe63f211ca
Georg Brandl <georg@python.org>
2015-09-28 18:34:48 +0200

    remove unneeded feature

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,7 @@
 //! The main entry point and crate definitions.
 
 // let's use some unstable features :)
-#![feature(box_syntax, result_expect, dir_builder, path_ext, drain)]
-#![feature(convert, plugin)]
+#![feature(box_syntax, dir_builder, path_ext, drain, convert, plugin)]
 #![plugin(docopt_macros)]
 
 #[macro_use]
